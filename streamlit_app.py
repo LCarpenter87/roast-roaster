654f5678f1b7b231a2eec5a9b9dfbde3b22b7a23
Lisa <L.carpenter@outlook.com>
2024-12-12 18:52:06 +0000

    beautiful commit message ^_^

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,41 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+import base64
+import google.generativeai as genai
+
+st.title("🎈 Lets roast some roasts!")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Share your image of a roast dinner, and I'll let you know how awful it is!"
 )
+
+genai.configure(api_key=st.secrets['GEMINI_API_KEY'])
+def upload_to_gemini(path, mime_type=None):
+  file = genai.upload_file(path, mime_type=mime_type)
+  print(f"Uploaded file '{file.display_name}' as: {file.uri}")
+  return file
+
+
+textsi_1 = """You are a sassy critical professional Roaster! 
+              You will be given roast dinner photos, and you will \"roast that dinner\" 
+            you with funny and witty retorts commenting on the quality of the roast dinner!"""
+
+generation_config = {
+    "max_output_tokens": 8192,
+    "temperature": 2,
+    "top_p": 0.95,
+}
+
+model = genai.GenerativeModel(
+  model_name='gemini-2.0-flash-exp',
+  generation_config=generation_config,
+  system_instruction=textsi_1
+)
+
+my_file = st.file_uploader("Choose your roast dinner!", type=['jpg'], accept_multiple_files=False )
+
+if my_file is not None:
+  gemini_file = upload_to_gemini(my_file, 'image/jpeg')
+  response = model.generate_content([gemini_file, "Roast this Roast!"])
+  st.write(response.text)
+  st.image(my_file)
+
